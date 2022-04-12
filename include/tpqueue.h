9500ac5d481e1a8b04f3e686631e599fafa29ec4
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-04-13 00:07:59 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,26 +4,26 @@
 
 template<typename T, int size>
 class TPQueue {
-    private:
-  T arr[100];
-  int first, last;
-    public:
-  TPQueue() : first(0), last(0) {}
-  void push(T x) {
-  int param = last++;
-  while ((--param >= first) && (arr[param % size].prior <= temp.prior)) {
-  arr[(param + 1) % size] = arr[param % size];
-  }
-  arr[(param + 1) % size] = x;
-  }
-  T pop() {
-  return arr[(first++)%size];
-  }
+ private:
+T arr[100];
+int first, last;
+ public:
+TPQueue() : first(0), last(0) {}
+void push(T x) {
+int param = last++;
+while ((--param >= first) && (arr[param % size].prior < temp.prior)) {
+arr[(param + 1) % size] = arr[param % size];
+}
+arr[(param + 1) % size] = x;
+}
+T pop() {
+return arr[(first++)%size];
+}
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
